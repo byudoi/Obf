bcc6844e673ab58faf5774c4b8f1db0f402af12b
byudoi <badfac3z@gmail.com>
2026-05-17 12:42:57 -0600

    Delete hercules directory

--- hercules/modules/watermark.lua
+++
@@ -1,8 +0,0 @@
--- modules/watermark.lua
-local Watermark = {}
-
-function Watermark.process(code)
-    return "-- obfuscated by y8y9 obf https://discord.gg/2DQbVrXJ8A\n" .. code
-end
-
-return Watermark
